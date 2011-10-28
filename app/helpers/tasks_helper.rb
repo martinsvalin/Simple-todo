fb0870ceb5b59f780cace75795c3b314d9089cd4
Martin Svalin <martin@lite.nu>
2011-10-28 19:39:27 +0200

    Add some color, and move completion mark

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,7 +2,7 @@
 module TasksHelper
   def completed(task)
     task.completed ?
-      "✔" :
-      "✖"
+      content_tag(:span, "✔", class: "completed") :
+      content_tag(:span, "✖", class: "incomplete")
   end
 end
--- spec/helpers/tasks_helper_spec.rb
+++ spec/helpers/tasks_helper_spec.rb
@@ -5,12 +5,12 @@ describe TasksHelper do
   describe "completed" do
     it "returns a check mark if the task is completed" do
       task = mock_model("Task", completed: true)
-      completed(task).should == "✔"
+      completed(task).should == content_tag(:span, "✔", class: "completed")
     end
 
     it "returns a cross if the task is completed" do
       task = mock_model("Task", completed: false)
-      completed(task).should == "✖"
+      completed(task).should == content_tag(:span, "✖", class: "incomplete")
     end
   end
 end
