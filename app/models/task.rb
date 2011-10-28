90bc03fd7ce1c4c5b189a1dbb5f48ea45efed414
Martin Svalin <martin@lite.nu>
2011-10-28 20:36:38 +0200

    Task can toggle completion with an appropriate message

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ActiveRecord::Base
   validates :name, :presence => true
+
+  def toggle_and_give_message
+    toggle(:completed)
+    save
+    completed ? "Task done!" : "Task undone."
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,14 +3,30 @@ require 'spec_helper'
 describe Task do
   context "validations" do
     # A simple, but naive spec
-    subject { Task.new :name => nil }
+    subject { Task.new name: nil }
     it { should_not be_valid }
 
     # A more specific spec
     it "should require name" do
-      lambda {
-        Task.create!(:name => nil)
-      }.should raise_error ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank"
+      expect {
+        Task.create!(name: nil)
+      }.to raise_error ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank"
+    end
+  end
+
+  describe "toggle_and_give_message" do
+    it "should toggle incomplete task to done" do
+      task = Task.create name: "Do something", completed: false
+      message = task.toggle_and_give_message
+      task.completed.should be_true
+      message.should == "Task done!"
+    end
+
+    it "should toggle undo a completed task" do
+      task = Task.create name: "Already did something", completed: true
+      message = task.toggle_and_give_message
+      task.completed.should be_false
+      message.should == "Task undone."
     end
   end
 end
