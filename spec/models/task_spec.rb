c0ef79924d033ce93f77ef8aac927fce40b98eaf
Martin Svalin <martin@lite.nu>
2011-10-28 18:44:01 +0200

    Failing spec for name validation

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Task do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    # A simple, but naive spec
+    subject { Task.new :name => nil }
+    it { should_not be_valid }
+
+    # A more specific spec
+    it "should require name" do
+      lambda {
+        Task.create!(:name => nil)
+      }.should raise_error ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank"
+    end
+  end
 end
