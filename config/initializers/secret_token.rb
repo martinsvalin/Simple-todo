bb065e29f042de37020aa4712ec6be831c81b2d5
Martin Svalin <martin@lite.nu>
2011-10-28 18:16:08 +0200

    New rails app
    Used RailsWizard to create a new app. I chose Rspec and Capybara  for unit testing and acceptance testing.
    rails new simple_todo -m http://railswizard.org/fb5d7b0cac8a2413f78d.rb -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTodo::Application.config.secret_token = '5b742d629416a149a33aabbcd76fd976db3b6c0bb45b454cc0247bfe7110da23f44a8fff0acca01474101fddb5a873320e9ab2a9dde6992e22917ad6949e1abc'
